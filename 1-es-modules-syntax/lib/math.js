f31a0d12c8a9d26b454216d15ef4a6eb5009bb41
mayinx <mayinxx@web.de>
2021-07-07 11:31:14 +0200

    Add solution

---
+++ 1-es-modules-syntax/lib/math.js
@@ -0,0 +1,17 @@
+export default function sum(a, b) {
+  return a + b;
+}
+
+export function substract(a, b) {
+  return a - b;
+}
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+export function divide(a, b) {
+  return a / b;
+}
+
+export const hehaheha = "südkorea";
--- 1-es-modules-syntax/main.js
+++ 1-es-modules-syntax/main.js
@@ -1,20 +1,18 @@
-function sum(a, b) {
-  return a + b;
-}
-
-function substract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+// Importing specific functions / variables from a module
+import sum, { substract, multiply, divide } from "./lib/math.js";
+import { hehaheha as suedkorea } from "./lib/math.js";
 
 console.log(sum(4, 5));
 console.log(substract(8, 4));
 console.log(multiply(6, 4));
 console.log(divide(8, 2));
+console.log(suedkorea);
+
+// Alt: Include the whole module (including default + named export) in one go
+// import sum, * as MyElaboratedMath from "./lib/math.js";
+
+// console.log(sum(4, 5));
+// console.log(MyElaboratedMath.substract(8, 4));
+// console.log(MyElaboratedMath.multiply(6, 4));
+// console.log(MyElaboratedMath.divide(8, 2));
+// console.log(MyElaboratedMath.hehaheha);
